a73f2407a6ba85d77d723cdd00bddc12f7ad3a8f
londonappbrewery <londonappbrewery@gmail.com>
2016-01-15 20:02:09 +0000

    Initial commit

---
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -0,0 +1,30 @@
+package com.londonappbrewery.destini;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    // TODO: Steps 4 & 8 - Declare member variables here:
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        // TODO: Step 5 - Wire up the 3 views from the layout to the member variables:
+
+
+
+        // TODO: Steps 6, 7, & 9 - Set a listener on the top button:
+
+
+
+
+        // TODO: Steps 6, 7, & 9 - Set a listener on the bottom button:
+
+
+    }
+}
